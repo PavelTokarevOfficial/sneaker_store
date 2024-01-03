d849b809e92c94d11c7913873a3d09ff2706ef22
PavelTokarevOfficial <64586673+PavelTokarevOfficial@users.noreply.github.com>
2024-01-03 15:32:37 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="scene">
+    <Spline
+        :scene="scene"
+
+    />
+  </div>
+</template>
+
+<script>
+import {defineComponent} from 'vue';
+import Spline from 'spline-vue/v3';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Spline,
+  },
+  data() {
+    return {
+      scene: 'https://prod.spline.design/5N00RXIsFyO9MIFf/scene.splinecode',
+    };
+  },
+});
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
